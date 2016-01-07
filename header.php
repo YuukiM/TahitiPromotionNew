c9bfabf9a33b8837d4d30a0d6727922a02ea689b
myuki <myuki@digitalriver.com>
2016-01-07 16:17:21 +0900

    favicon url

--- header.php
+++ header.php
@@ -11,7 +11,25 @@
     <?php endif; ?>
     <meta name="description" content="タヒチプロモーションでは本場のタヒチにこだわり、迫力あるショーやバラエティー出演にいたるまで、枠にとらわれることなく、素晴らしいタヒチアンダンスを披露するダンサーを揃えています。" />
     <meta name="keywords" content="tahiti promotion、タヒチプロモーション,タヒチ,タヒチアンダンス,テ ラ キョウコ,TAHITI,TAHITIAN,TAHITIAN DANCE,タウルミ,taurumi,tahaki,タハキ,タヒチ プロモーション,DANCE" />
-
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo("template_url"); ?>/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="<?php bloginfo("template_url"); ?>/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/<?php bloginfo("template_url"); ?>/faviconsfavicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="<?php bloginfo("template_url"); ?>/favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?php bloginfo("template_url"); ?>/favicons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="<?php bloginfo("template_url"); ?>/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php bloginfo("template_url"); ?>/favicons/manifest.json">
+    <link rel="mask-icon" href="<?php bloginfo("template_url"); ?>/favicons/safari-pinned-tab.svg" color="#171f55">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?php bloginfo("template_url"); ?>/favicons/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <!-- Bootstrap -->
     <link href="<?php bloginfo("template_url")?>/style.css" rel="stylesheet">
 
