285bed0aef6892991d3407b95c303f290a427f53
YuukiM <yuuki@m-yuuki.com>
2016-01-07 22:06:04 +0900

    Event Artist file added

---
+++ event-artist.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Template Name: Event Artist
+*/
+?>
+
+<?php get_header(); ?>
+    <div id="main">
+        <div class="container">
+            <div class="row">
+            </div>
+        </div><!-- /.container -->
+    </div>
+<?php get_footer(); ?>
