7fde9267eefa924034d2a54b6442982c0d3361f0
YuukiM <yuuki@m-yuuki.com>
2016-01-10 14:47:00 +0900

    Event Artist if

--- event-artist.php
+++ event-artist.php
@@ -8,6 +8,18 @@
     <div id="main">
         <div class="container">
             <div class="row">
+                <?php
+                    $args = array(
+                        "post_type" => "events",
+                        "meta_key" => "order",
+                        "orderby" => "meta_value_num",
+                        "order" => "ASC",
+                        "numberposts" => "0"
+                    );
+                    get_posts($args);
+                    
+
+                ?>
             </div>
         </div><!-- /.container -->
     </div>
