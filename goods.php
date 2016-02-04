aeb0ef255ad68821f10d483aa0eb584bcd418582
YuukiM <yuuki@m-yuuki.com>
2016-02-04 23:11:49 +0900

    New PHP (goods)

--- goods.php
+++ goods.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Template Name: page
+    Template Name: Goods
 */
 ?>
 
@@ -9,34 +9,16 @@
         <div class="container">
             <div class="row">
                 <?php
-                if (is_page(34) || is_page(36)) { //34 freepaper, 36 goods
                     $leadText = get_field("leadText", $id);
-
-                    if (is_page(34)){
-                        $Args = array(
-                            'post_type' => 'tahiti-japon',
-                            'posts_per_page' => 1,
-                            'orderby' => 'name',
-                            'order' => 'DESC'
-                        );
-                    }
-                    if (is_page(36)){
-                        $Args = array(
-                            'post_type' => 'goods',
-                            'posts_per_page' => 2,
-                            'orderby' => 'date',
-                            'order' => 'DESC'
-                        );
-                    }
+                    $Args = array(
+                        'post_type' => 'goods',
+                        'posts_per_page' => 2,
+                        'orderby' => 'date',
+                        'order' => 'DESC'
+                    );
                     $item = new WP_Query($Args);
                     if ($item->have_posts()) :
-                        if(is_page(34)) {
-                            echo "<div class=\"col-xs-12\" id=\"left\">";
-                        }
-                        if(is_page(36)) {
-                            echo "<div class=\"col-xs-12 col-md-10\" id=\"left\">";
-                            /*echo "<div class='lead-text'><h1>".get_field('leadText', get_the_ID())."</h1></div>";*/
-                        }
+                        echo "<div class=\"col-xs-12 col-md-10\" id=\"left\">";
                         while ($item->have_posts()) {
                             if ($leadText) {
                                 echo "<div class=\"lead-text\"><h1>" . $leadText . "</h1></div>";
@@ -52,42 +34,27 @@
                             echo "<div class=\"row\">";
                             the_title("<div class=\"col-xs-12\"><h2>","</h2></div>");
                             echo "<section class=\"main-content col-xs-12\">";
-                                echo "<div class=\"row\">";
-                                    echo "<div class=\"col-xs-12 col-sm-3 col-md-4\">";
-                                        if ($thumbnailL) {
-                                            if(is_page(34)) {
-                                                echo "<div class='free-paper'><a href=\"" . $pdf . "\"><img src=\"" . $thumbnailL . "\"/></a></div>";
-                                            }
-                                            if(is_page(36)) {
-                                                echo "<div class='free-paper'><a href=\"" . $linkURL . "\"><img src=\"" . $thumbnailL . "\"/></a></div>";
-                                            }
-                                        } else {
-                                            echo "No image.";
-                                        }
-                                    echo "</div>";
-                                    echo "<article class=\"col-xs-12 col-sm-9 col-md-8\">";
-                                        if(is_page(36)){
-                                            //the_title("<h1>","</h1>");
-                                            if ($itemContent) {
-                                                the_content();
-                                                echo "<a href='".$linkURL."'>この商品の詳しい情報はこちら</a>";
-                                            }else{
-                                                echo "Coming soon";
-                                            }
-                                        }else{
-                                            if ($itemContent) {
-                                                the_content();
-                                            }else{
-                                                echo "Coming soon";
-                                            }
-                                        }
-                                    echo "</article>";
-                                echo "</div>";
+                            echo "<div class=\"row\">";
+                            echo "<div class=\"col-xs-12 col-sm-3 col-md-4\">";
+                            if ($thumbnailL) {
+                                echo "<div class='free-paper'><a href=\"" . $linkURL . "\"><img src=\"" . $thumbnailL . "\"/></a></div>";
+                            } else {
+                                echo "No image.";
+                            }
+                            echo "</div>";
+                            echo "<article class=\"col-xs-12 col-sm-9 col-md-8\">";
+                            if ($itemContent) {
+                                the_content();
+                                echo "<a href='".$linkURL."'>この商品の詳しい情報はこちら</a>";
+                            }else{
+                                echo "Coming soon";
+                            }
+                            echo "</article>";
+                            echo "</div>";
                             echo "</section>";
                             echo "</div>";
                         }
-                    ?>
-                        <?php if(is_page(36)): ?>
+                        ?>
                         <div class="row">
                             <div class="lead-text col-xs-12"><h1>その他の商品カテゴリー</h1></div>
                             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
@@ -217,37 +184,11 @@
                                 </div>
                             </div>
                         </div>
-                    <?php endif;
+                    <?php
                         echo "</div>";
                     endif;
                     wp_reset_postdata();
-
-                } else {
-                    if (have_posts()) {
-                        while (have_posts()) {
-                            the_post();
-                            $leadText = get_field("leadText", $id);
-                            if ($leadText) {
-                                echo "<div class=\"lead-text col-xs-12\"><h1>" . $leadText . "</h1></div>";
-                            }
-                            else{
-                                echo "<div class=\"lead-text col-xs-12\"><h1>" . get_the_title() . "</h1></div>";
-                            }
-                            echo "<div class=\"col-xs-12 col-md-9\" id=\"left\">";
-                            if (get_the_content()) {
-                                echo "<section class='main-content'>";
-                                    echo "<article>";
-                                        the_content();
-                                    echo "</article>";
-                                echo "</section>";
-                            }
-                            echo "</div>";
-                        }
-                    } else {
-                        echo "<p>" . _e('Sorry, no posts matched your criteria.') . "</p>";
-                    }
-                }
-                get_sidebar();
+                    get_sidebar();
                 ?>
             </div>
         </div><!-- /.container -->
